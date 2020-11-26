29b13814882b3ddb3c037de38a412a4bb7f413c6
Serhii <ya.motr@gmail.com>
2020-11-26 15:58:25 +0200

    1

---
+++ Module_Unity/Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public GameObject explosion;
+    public AudioSource explosionSound;
+    private GameObject clone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionSound = GameObject.Find("ExplosionSound").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        explosionSound.Play();
+        clone = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Destroy(clone, 2f);
+    }
+
+}
---
+++ Module_Unity/Assets/Scripts/Fire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject prefabBullet;
+    public GameObject clonePosition;
+    public AudioSource audioSource;
+
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+            GameObject clone = Instantiate(prefabBullet);
+            clone.transform.position = clonePosition.transform.position;
+            clone.GetComponent<Rigidbody>().AddForce(clonePosition.transform.forward * force, ForceMode.Impulse);
+        }
+    }
+}
